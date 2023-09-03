a5b28b97e6e6883fe6c136eec118fdf9a1f61ba7
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-09-03 11:55:00 +0800

    feat: log results before and after

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { readFile, writeFile } from "node:fs/promises";
 
 program
 	.name("sarif-strip-suppressed")
-	.version("0.1.0", "-v, --version")
+	.version("0.2.0", "-v, --version")
 	.description("Strips suppressed results from SARIF files");
 
 program.argument("file");
@@ -17,9 +17,15 @@ const file = program.args[0];
 const fileContents = await readFile(file, { encoding: "utf-8" });
 const fileData = JSON.parse(fileContents) as { runs: Run[] };
 
-for (const run of fileData.runs) {
+for (let runIdx = 0; runIdx < fileData.runs.length; runIdx++) {
+	const run = fileData.runs[runIdx];
+
 	if (run.results) {
+		const before = run.results.length;
 		run.results = run.results.filter((res) => !res.suppressions || res.suppressions.length === 0);
+		const after = run.results.length;
+
+		console.log(`Run #${runIdx}: ${before} -> ${after}`);
 	}
 }
 
